8f038bc0eb3000ffe1fc04b4312efa031650c563
Marius Andra <marius.andra@gmail.com>
2023-07-29 00:59:00 +0300

    actually initialized something

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, jsonify, request, abort, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from paramiko import SSHClient, AutoAddPolicy
+from flask_socketio import SocketIO, emit
+from paramiko import RSAKey, SSHClient, AutoAddPolicy
 from scp import SCPClient
+from io import StringIO
 import requests
 
 LOCAL_VERSION = "1.0.0"  # You should update this constant whenever you have a new version
 
 app = Flask(__name__, static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/frameos.db'
 db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Initializing SocketIO with the Flask app
 
 class Frame(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -85,6 +88,7 @@ def check_update(id):
 
 @app.route('/frames/<int:id>/initialize', methods=['POST'])
 def initialize_frame(id):
+    socketio.emit('initialize_frame_output', {'data': "Starting..."}, namespace='/test')
     frame = Frame.query.get_or_404(id)
     ssh_user = request.form['ssh_user']
     ssh_pass = request.form.get('ssh_pass')
@@ -93,15 +97,20 @@ def initialize_frame(id):
     ssh = SSHClient()
     ssh.set_missing_host_key_policy(AutoAddPolicy())
     
+    socketio.emit('initialize_frame_output', {'data': f"Connecting to {ssh_user}@{frame.ip}"}, namespace='/test')
+
     if ssh_key:
-        ssh.connect(frame.ip, username=ssh_user, key_filename=ssh_key, timeout=10)
+        ssh_key_obj = RSAKey.from_private_key(StringIO(ssh_key))
+        ssh.connect(frame.ip, username=ssh_user, pkey=ssh_key_obj, timeout=10)
     else:
         ssh.connect(frame.ip, username=ssh_user, password=ssh_pass, timeout=10)
 
     with SCPClient(ssh.get_transport()) as scp:
         scp.put('../client/frame.py', 'frame.py')
 
-    ssh.exec_command('python frame.py')
+    _, stdout, _ = ssh.exec_command('python frame.py')
+    for line in stdout:
+        socketio.emit('initialize_frame_output', {'data': line}, namespace='/test')
 
     frame.status = "initialized"
     db.session.commit()
@@ -116,4 +125,5 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
