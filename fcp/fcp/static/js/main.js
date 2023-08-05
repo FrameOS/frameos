588c805812960c64e9321e03a3420337900be36a
Marius Andra <marius.andra@gmail.com>
2023-08-05 23:42:22 +0300

    nicer styles

--- fcp/fcp/static/js/main.js
+++ fcp/fcp/static/js/main.js
@@ -1,8 +1,19 @@
 var socket = io.connect("http://0.0.0.0:8080");
 socket.on("new_line", function (msg) {
-  var li = document.createElement("li");
-  li.className = msg.type;
-  li.textContent = msg.line;
-  document.getElementById("log").appendChild(li);
+  console.log(msg);
+  var line = document.createElement("div");
+  line.className = "terminal-line " + msg.type;
+
+  var timestamp = document.createElement("span");
+  timestamp.className = "terminal-timestamp";
+  timestamp.textContent = msg.timestamp;
+  line.appendChild(timestamp);
+
+  var text = document.createElement("span");
+  text.className = "terminal-text";
+  text.textContent = msg.line;
+  line.appendChild(text);
+
+  document.getElementById("log").appendChild(line);
   window.scrollTo(0, document.body.scrollHeight);
 });
--- fcp/fcp/tasks.py
+++ fcp/fcp/tasks.py
@@ -8,14 +8,14 @@ def log(type: str, line: str) -> None:
         frame_log = models.SSHLog(frame_id=0, line=line, type=type)
         db.session.add(frame_log)
         db.session.commit()
-        socketio.emit('new_line', {'line': line, 'type': type})
+        socketio.emit('new_line', {'line': line, 'type': type, 'timestamp': str(frame_log.timestamp)})
 
 
 @huey.task()
 def initialize_frame():
     with app.app_context():
         try:
-            log("stdout", "Connecting to marius@devbox")
+            log("stdinfo", "Connecting to marius@devbox")
 
             ssh = SSHClient()
             ssh.set_missing_host_key_policy(AutoAddPolicy())
@@ -28,7 +28,7 @@ def initialize_frame():
             # else:
             #     ssh.connect(frame.ip, username=ssh_user, password=ssh_pass, timeout=10)
             
-            log("stdout", "Connected to marius@devbox")
+            log("stdinfo", "Connected to marius@devbox")
             
             stdin, stdout, stderr = ssh.exec_command("df -h")
 
