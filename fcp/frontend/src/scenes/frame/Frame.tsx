9f069086c4321cf935ddf061eba31e34ed3eb9fb
Marius Andra <marius.andra@gmail.com>
2023-08-06 19:15:39 +0300

    frame view basics

--- fcp/frontend/src/scenes/frame/Frame.tsx
+++ fcp/frontend/src/scenes/frame/Frame.tsx
@@ -2,6 +2,8 @@ import { useValues } from 'kea'
 import { H1 } from '../../components/H1'
 import { frameLogic } from './frameLogic'
 import { A } from 'kea-router'
+import { Box } from '../../components/Box'
+import { H6 } from '../../components/H6'
 
 interface FrameSceneProps {
   id: string // from the URL
@@ -12,10 +14,20 @@ export function Frame(props: FrameSceneProps) {
   const { frame, frameLoading } = useValues(frameLogic(logicProps))
 
   return (
-    <div>
+    <div className="space-y-4">
       <H1>
         <A href="/">FrameOS</A> <span className="text-gray-400">&raquo;</span> {frameLoading ? '...' : frame.ip}
       </H1>
+      {frame ? (
+        <>
+          <Box className="text-center">
+            <img className="rounded-t-lg" src={`/images/image${(frame.id % 20) + 1}.jpg`} alt="" />
+          </Box>
+          <Box className="p-4">
+            <H6>SSH Logs</H6>
+          </Box>
+        </>
+      ) : null}
     </div>
   )
 }
