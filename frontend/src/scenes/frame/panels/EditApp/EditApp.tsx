5ba0f0f9d288d8e6eee1a8b7fe41912c339f63bf
Marius Andra <marius.andra@gmail.com>
2023-09-29 23:59:23 +0200

    bit better

--- frontend/src/scenes/frame/panels/EditApp/EditApp.tsx
+++ frontend/src/scenes/frame/panels/EditApp/EditApp.tsx
@@ -10,23 +10,18 @@ export function EditApp({ keyword }: EditAppProps) {
   const { sources, sourcesLoading, activeFile } = useValues(editAppLogic({ keyword }))
   const { setActiveFile } = useActions(editAppLogic({ keyword }))
   return (
-    <div className="space-y-2">
-      <div className="font-bold">Built-in apps are view-only. Forking and editing of custom apps coming soon.</div>
-      <div className="flex flex-row">
-        <div>
-          {Object.entries(sources).map(([file, source]) => (
-            <div key={file} className="w-min">
-              <Button size="small" color="none" onClick={() => setActiveFile(file)}>
-                {file}
-              </Button>
-            </div>
-          ))}
-        </div>
-        <div>
-          <div className="bg-black p-4 font-mono text-sm overflow-y-scroll overflow-x-hidden w-max">
-            <pre>{sources[activeFile] ?? sources[Object.keys(sources)[0]] ?? ''}</pre>
+    <div className="flex flex-row max-h-full h-full max-w-full w-full">
+      <div>
+        {Object.entries(sources).map(([file, source]) => (
+          <div key={file} className="w-min">
+            <Button size="small" color={activeFile === file ? 'teal' : 'none'} onClick={() => setActiveFile(file)}>
+              {file}
+            </Button>
           </div>
-        </div>
+        ))}
+      </div>
+      <div className="bg-black p-4 font-mono text-sm overflow-y-scroll overflow-x-scroll w-full">
+        <pre>{sources[activeFile] ?? sources[Object.keys(sources)[0]] ?? ''}</pre>
       </div>
     </div>
   )
