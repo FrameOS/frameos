391040ad74b99a73d452b12ef1b78848ab002ecd
Marius Andra <marius.andra@gmail.com>
2023-09-30 00:36:38 +0200

    tweaks

--- frontend/src/scenes/frame/panels/EditApp/EditApp.tsx
+++ frontend/src/scenes/frame/panels/EditApp/EditApp.tsx
@@ -9,9 +9,12 @@ interface EditAppProps {
 export function EditApp({ keyword }: EditAppProps) {
   const { sources, sourcesLoading, activeFile } = useValues(editAppLogic({ keyword }))
   const { setActiveFile } = useActions(editAppLogic({ keyword }))
+  if (sourcesLoading) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="flex flex-row max-h-full h-full max-w-full w-full">
-      <div>
+      <div className="max-w-40">
         {Object.entries(sources).map(([file, source]) => (
           <div key={file} className="w-min">
             <Button size="small" color={activeFile === file ? 'teal' : 'none'} onClick={() => setActiveFile(file)}>
@@ -20,7 +23,7 @@ export function EditApp({ keyword }: EditAppProps) {
           </div>
         ))}
       </div>
-      <div className="bg-black p-4 font-mono text-sm overflow-y-scroll overflow-x-scroll w-full">
+      <div className="bg-black p-4 font-mono text-sm overflow-y-auto overflow-x-auto w-full">
         <pre>{sources[activeFile] ?? sources[Object.keys(sources)[0]] ?? ''}</pre>
       </div>
     </div>
