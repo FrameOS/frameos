edeba89b007443e326d09fb7f82be9d4542e46a0
Marius Andra <marius.andra@gmail.com>
2023-10-13 00:14:21 +0200

    tabs scroll if no other way

--- frontend/src/components/panels/Tabs.tsx
+++ frontend/src/components/panels/Tabs.tsx
@@ -9,7 +9,7 @@ export function Tabs({ children, className }: TabsProps): JSX.Element {
   return (
     <div
       className={clsx(
-        'flex items-start flex-nowrap text-sm font-medium text-center text-gray-500 dark:border-gray-700 dark:text-gray-400 space-x-2 w-auto',
+        'flex items-start flex-nowrap text-sm font-medium text-center text-gray-500 dark:border-gray-700 dark:text-gray-400 space-x-2 w-auto overflow-y-auto',
         className
       )}
     >
