aae8628480babedddb20f6c04669063b2a5ce707
Marius Andra <marius.andra@gmail.com>
2023-10-14 02:35:27 +0200

    more events

--- frontend/src/scenes/frame/panels/Events/Events.tsx
+++ frontend/src/scenes/frame/panels/Events/Events.tsx
@@ -1,6 +1,13 @@
 import { Box } from '../../../../components/Box'
 import { H6 } from '../../../../components/H6'
 
+const events = {
+  render: "Render the screen. Triggered according to the frame's refresh interval.",
+  button_press: 'When a button is pressed',
+  touch_press: 'When a touch screen is pressed',
+  mouse_click: 'When a mouse is clicked',
+}
+
 export function Events() {
   const onDragStart = (event: any, type: 'event', keyword: string) => {
     event.dataTransfer.setData('application/reactflow', JSON.stringify({ type, keyword }))
@@ -10,27 +17,18 @@ export function Events() {
     <div className="space-y-2">
       <div>Events can be dispatched or listened to. Connect either end.</div>
 
-      <Box
-        className="bg-gray-900 px-3 py-2 dndnode flex items-center justify-between space-x-2 cursor-move"
-        draggable
-        onDragStart={(event) => onDragStart(event, 'event', 'render')}
-      >
-        <div>
-          <H6>Event: Render</H6>
-          <div className="text-sm">Render the screen. Triggered according to the frame's refresh interval.</div>
-        </div>
-      </Box>
-
-      <Box
-        className="bg-gray-900 px-3 py-2 dndnode flex items-center justify-between space-x-2 cursor-move"
-        draggable
-        onDragStart={(event) => onDragStart(event, 'event', 'button_press')}
-      >
-        <div>
-          <H6>Event: Button Press</H6>
-          <div className="text-sm">When a button or screen is pressed</div>
-        </div>
-      </Box>
+      {Object.entries(events).map(([keyword, description]) => (
+        <Box
+          className="bg-gray-900 px-3 py-2 dndnode flex items-center justify-between space-x-2 cursor-move"
+          draggable
+          onDragStart={(event) => onDragStart(event, 'event', keyword)}
+        >
+          <div>
+            <H6>Event: {keyword}</H6>
+            <div className="text-sm">{description}</div>
+          </div>
+        </Box>
+      ))}
     </div>
   )
 }
