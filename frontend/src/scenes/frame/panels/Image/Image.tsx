053d1119ad6bd6d8f26ea9ac40d211b93112e8de
Marius Andra <marius.andra@gmail.com>
2023-10-17 23:59:15 +0200

    refresh

--- frontend/src/scenes/frame/panels/Image/Image.tsx
+++ frontend/src/scenes/frame/panels/Image/Image.tsx
@@ -1,27 +1,43 @@
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import clsx from 'clsx'
 import { framesModel } from '../../../../models/framesModel'
 import { frameLogic } from '../../frameLogic'
+import { useEffect, useState } from 'react'
 
-export interface ImageProps {
+export interface ImageProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string
 }
 
-export function Image({ className }: ImageProps) {
+export function Image({ className, ...props }: ImageProps) {
   const { getFrameImage, frames } = useValues(framesModel)
+  const { updateFrameImage } = useActions(framesModel)
   const { id } = useValues(frameLogic)
+
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setIsLoading(true)
+  }, [getFrameImage(id)])
+
+  const visiblyLoading = (isLoading || frames[id]?.status !== 'ready') && frames[id]?.interval > 5
+
   return (
     <div
       className={clsx(
         'max-w-full max-h-full w-full h-full flex items-center justify-center',
-        frames[id]?.status !== 'ready' && frames[id]?.interval > 5 ? 'continuous-fade-in-out' : null,
+        visiblyLoading ? 'continuous-fade-in-out' : null,
+        visiblyLoading ? 'cursor-wait' : 'cursor-pointer',
         className
       )}
+      onClick={() => updateFrameImage(id)}
+      title="Click to refresh"
+      {...props}
     >
       {frames[id] ? (
         <img
-          className="rounded-lg rounded-tl-none max-w-full max-h-full"
+          className={clsx('rounded-lg rounded-tl-none max-w-full max-h-full')}
           src={getFrameImage(id)}
+          onLoad={() => setIsLoading(false)}
+          onError={() => setIsLoading(false)}
           style={{
             ...(frames[id].width && frames[id].height
               ? {
