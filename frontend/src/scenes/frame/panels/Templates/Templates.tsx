970884b217752723d0bed9ef40a25ee6932fde30
Marius Andra <marius.andra@gmail.com>
2023-10-18 21:23:07 +0200

    template design

--- frontend/src/scenes/frame/panels/Templates/Templates.tsx
+++ frontend/src/scenes/frame/panels/Templates/Templates.tsx
@@ -62,19 +62,26 @@ export function Templates() {
         <div className="space-y-2">
           <H6>Local templates</H6>
           {templates.map((template) => (
-            <Box
-              className="bg-gray-900 dndnode relative"
+            <div
+              className="shadow bg-gray-900 break-inside-avoid dndnode relative rounded-lg"
               style={
                 template.image_width && template.image_height
                   ? {
                       backgroundImage: `url("/api/templates/${template.id}/image")`,
                       backgroundSize: 'cover',
+                      backgroundPosition: 'center',
                       aspectRatio: `${template.image_width} / ${template.image_height}`,
                     }
                   : {}
               }
             >
-              <div className="w-full h-full p-3 space-y-2" style={{ background: 'rgba(0,0,0,0.5)' }}>
+              <div
+                className="w-full h-full p-3 space-y-2 rounded-lg border border-gray-700"
+                style={{
+                  backgroundImage:
+                    'linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.5) 30%, rgba(0, 0, 0, 0.6) 70%, rgba(0, 0, 0, 0.7) 100%)',
+                }}
+              >
                 <div className="flex items-center justify-between">
                   <H6>{template.name}</H6>
                   <div className="flex items-center gap-2">
@@ -118,7 +125,7 @@ export function Templates() {
                 </div>
                 {template.description && <div className="text-white text-sm">{template.description}</div>}
               </div>
-            </Box>
+            </div>
           ))}
           {templates.length === 0 ? <div className="text-muted">You have no local templates.</div> : null}
         </div>
