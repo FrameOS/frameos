35530d500e9823b74ecabbc9a9beb1b95a67e560
Marius Andra <marius.andra@gmail.com>
2023-11-20 22:03:02 +0100

    get most tests to run

--- backend/app/tests/base.py
+++ backend/app/tests/base.py
@@ -1,4 +1,5 @@
 import unittest
+
 from flask import Flask
 from app import create_app, db
 from app.models import User
@@ -11,34 +12,26 @@ class BaseTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.app = create_app(TestConfig)
         cls.client = cls.app.test_client()
-        with cls.app.app_context():
-            db.session.rollback()
-            db.drop_all()
-            db.create_all()
-        # cls.setUp(cls)
-
-    @classmethod
-    def tearDownClass(cls):
-        with cls.app.app_context():
-            db.session.rollback()
-            db.session.remove()
-            db.drop_all()
 
     def setUp(self):
-        # Set up a test request context for each test
         self.app_context = self.app.test_request_context()
         self.app_context.push()
+        db.drop_all()
+        db.create_all()
         self.create_user("tester", "test@example.com", "testpassword")
 
     def tearDown(self):
-        db.session.rollback()
         self.app_context.pop()
 
     def create_user(self, username, email, password):
-        user = User(username=username, email=email)
-        user.set_password(password)
-        db.session.add(user)
-        db.session.commit()
+        try:
+            user = User(username=username, email=email)
+            user.set_password(password)
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise
 
     def login(self, username, password):
         return self.client.post('/api/login', data=dict(
