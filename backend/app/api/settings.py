91c4bce1378c54c06987515e8876ad438e6c8c7a
Marius Andra <marius.andra@gmail.com>
2023-11-25 16:30:34 +0100

    tests for settings

--- backend/app/api/settings.py
+++ backend/app/api/settings.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 from flask_login import login_required
+from sqlalchemy.exc import SQLAlchemyError
 from app import db
 from . import api
 from app.models.settings import get_settings_dict, Settings
@@ -12,21 +13,23 @@ def settings():
 @api.route("/settings", methods=["POST"])
 @login_required
 def set_settings():
-    current_settings = get_settings_dict()
-
     payload = request.get_json()
     if not payload:
         return jsonify(error="No JSON payload received"), 400
 
-    for key, value in payload.items():
-        if value != current_settings.get(key, None):
-            if key in current_settings:
-                setting = Settings.query.filter_by(key=key).first()
-                setting.value = value
-            else:
-                setting = Settings(key=key, value=value)
-                db.session.add(setting)
-    db.session.commit()
+    try:
+        current_settings = get_settings_dict()
+        for key, value in payload.items():
+            if value != current_settings.get(key, None):
+                if key in current_settings:
+                    setting = Settings.query.filter_by(key=key).first()
+                    setting.value = value
+                else:
+                    setting = Settings(key=key, value=value)
+                    db.session.add(setting)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        return jsonify(error="Database error"), 500
 
     return jsonify(get_settings_dict())
 
@@ -36,10 +39,14 @@ def generate_ssh_keys():
     from cryptography.hazmat.primitives.asymmetric import rsa
     from cryptography.hazmat.primitives import serialization
 
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=3072,
-    )
+    try:
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=3072,
+        )
+    except Exception as e:
+        return jsonify(error="Key generation error"), 500
+
     public_key = private_key.public_key()
     private_key_bytes = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
---
+++ backend/app/api/tests/test_settings.py
@@ -0,0 +1,60 @@
+import json
+from unittest.mock import patch
+from app.tests.base import BaseTestCase
+
+class TestSettingsAPI(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def test_get_settings(self):
+        # Test the GET /settings endpoint
+        response = self.client.get('/api/settings')
+        self.assertEqual(response.status_code, 200)
+        settings = json.loads(response.data)
+        self.assertIsInstance(settings, dict)
+
+    def test_set_settings(self):
+        # Test the POST /settings endpoint
+        data = {'some_setting': 'new_value'}
+        response = self.client.post('/api/settings', json=data)
+        self.assertEqual(response.status_code, 200)
+        updated_settings = json.loads(response.data)
+        self.assertEqual(updated_settings.get('some_setting'), 'new_value')
+
+    def test_set_settings_no_payload(self):
+        # Test the POST /settings endpoint with no payload
+        response = self.client.post('/api/settings', json={})
+        self.assertEqual(response.status_code, 400)
+
+    def test_generate_ssh_keys(self):
+        # Test the POST /generate_ssh_keys endpoint
+        response = self.client.post('/api/generate_ssh_keys')
+        self.assertEqual(response.status_code, 200)
+        keys = json.loads(response.data)
+        self.assertIn('private', keys)
+        self.assertIn('public', keys)
+
+    def test_unauthorized_access(self):
+        self.logout()
+
+        endpoints = [
+            ('/api/settings', 'GET', None),
+            ('/api/settings', 'POST', {'some_setting': 'value'}),
+            ('/api/generate_ssh_keys', 'POST', None)
+        ]
+        for endpoint, method, data in endpoints:
+            response = self.client.open(endpoint, method=method, json=data)
+            self.assertEqual(response.status_code, 401)
+
+    def test_generate_ssh_keys_error_handling(self):
+        # Simulate an error during key generation
+        with patch('cryptography.hazmat.primitives.asymmetric.rsa.generate_private_key') as mock_generate:
+            mock_generate.side_effect = Exception("Key generation error")
+            response = self.client.post('/api/generate_ssh_keys')
+            self.assertEqual(response.status_code, 500)
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
