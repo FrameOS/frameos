28e29172efb937ec65ff22237e0c4bf306cccdc2
Marius Andra <marius.andra@gmail.com>
2023-11-28 09:58:42 +0100

    higher fidelity logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,18 +10,18 @@ interface HeaderProps {
 
 export function Header({ title, right, buttons }: HeaderProps) {
   return (
-    <div
+    <span
       className="bg-gray-800 text-white h-full w-full space-x-2 p-2 pt-3 px-4 flex justify-between items-center"
       style={{ height: 60 }}
     >
-      <H5 className="truncate">
-        <A href="/"><img src='/mark-white.svg' className="w-6 h-6 inline-block mr-2 align-baseline" alt="FrameOS" /></A>
-        {title}
-      </H5>
+      <div className="truncate flex items-center justify-center gap-3">
+        <A href="/"><img src='/img/logo/dark-mark-small.png' className="w-[28px] h-[28px] inline-block align-center" alt="FrameOS" /></A>
+        <H5>{title}</H5>
+      </div>
       <div className="flex space-x-2">
         {right && <div>{right}</div>}
         {buttons}
       </div>
-    </div>
+    </span>
   )
 }
