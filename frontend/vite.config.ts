0ca9da371356a6c9b9ebc0caf7ad787c3883e353
Marius Andra <marius.andra@gmail.com>
2023-11-30 23:53:12 +0100

    try to make builds faster

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          monaco: ['monaco-editor', '@monaco-editor/react'],
+        },
+      },
+    },
+  },
 })
