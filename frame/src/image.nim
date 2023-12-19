718b1d5856a03d60f9dfc174e2311ee40f075d1d
Marius Andra <marius.andra@gmail.com>
2023-12-19 23:25:11 +0100

    measure time

--- frame/src/image.nim
+++ frame/src/image.nim
@@ -2,6 +2,7 @@ import pixie
 import assets/fonts as fontAssets
 import httpclient
 import std/strformat
+import times
 
 proc downloadImage(url: string): Image =
   let client = newHttpClient()
@@ -12,19 +13,29 @@ proc downloadImage(url: string): Image =
     client.close()
 
 proc createImage(width, height: int): Image =
+  echo "---- Creating image ----"
   let image = newImage(width, height)
   let keyword = "random"
   let url = &"https://source.unsplash.com/random/{width}x{height}/?{keyword}"
-  let background = downloadImage(url)
-  image.draw(background)
 
+  let downloadTimer = epochTime()
+  let background = downloadImage(url)
+  echo "Time taken to downlooad: ", (epochTime() - downloadTimer) * 1000, " ms"
+
+  let drawTimer = epochTime()
+  image.draw(background)
+  echo "Time taken to draw background: ", (epochTime() - drawTimer) * 1000, " ms"
+
+  let parseTtfTimer = epochTime()
   let typeface = parseTtf(fontAssets.getAsset("assets/fonts/Ubuntu-Regular_1.ttf"))
+  echo "Time taken to parse ttf: ", (epochTime() - parseTtfTimer) * 1000, " ms"
 
   proc newFont(typeface: Typeface, size: float32, color: Color): Font =
     result = newFont(typeface)
     result.size = size
     result.paint.color = color
 
+  let drawFontTimer = epochTime()
   let spans = @[
     newSpan("verb [with object] ",
       newFont(typeface, 12, color(0.78125, 0.78125, 0.78125, 1))),
@@ -35,6 +46,7 @@ proc createImage(width, height: int): Image =
   ]
 
   image.fillText(typeset(spans, vec2(180, 180)), translate(vec2(10, 10)))
+  echo "Time taken to draw text: ", (epochTime() - drawFontTimer) * 1000, " ms"
   return image
 
 export createImage
