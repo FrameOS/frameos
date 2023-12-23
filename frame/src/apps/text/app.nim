0614a94643a5ab3424ca244e49d29de21064a041
Marius Andra <marius.andra@gmail.com>
2023-12-23 16:47:55 +0100

    app cleanup

---
+++ frame/src/apps/text/app.nim
@@ -0,0 +1,37 @@
+import pixie
+import assets/fonts as fontAssets
+import times
+from frameos/config import Config
+
+type AppConfig* = object
+  text*: string
+
+type App* = object
+  appConfig: AppConfig
+  config: Config
+  typeface: Typeface
+
+proc init*(config: Config, appConfig: AppConfig): App =
+  result = App(config: config, appConfig: appConfig)
+  let parseTtfTimer = epochTime()
+  result.typeface = parseTtf(fontAssets.getAsset("assets/fonts/Ubuntu-Regular_1.ttf"))
+  echo "Time taken to parse ttf: ", (epochTime() - parseTtfTimer) * 1000, " ms"
+
+proc render*(self: App, image: Image) =
+  proc newFont(typeface: Typeface, size: float32, color: Color): Font =
+    result = newFont(typeface)
+    result.size = size
+    result.paint.color = color
+
+  let drawFontTimer = epochTime()
+  let spans = @[
+    newSpan("verb [with object] ",
+      newFont(self.typeface, 12, color(0.78125, 0.78125, 0.78125, 1))),
+    newSpan("unladen strallow\n", newFont(self.typeface, 36, color(0, 0, 0, 1))),
+    newSpan("\nstral·low\n", newFont(self.typeface, 13, color(0, 0.5, 0.953125, 1))),
+    newSpan("\n1. free (something) from restrictive restrictions \"the regulations are intended to strallow changes in public policy\" ",
+        newFont(self.typeface, 14, color(0.3125, 0.3125, 0.3125, 1)))
+  ]
+
+  image.fillText(typeset(spans, vec2(180, 180)), translate(vec2(10, 10)))
+  echo "Time taken to draw text: ", (epochTime() - drawFontTimer) * 1000, " ms"
--- frame/src/scenes/default.nim
+++ frame/src/scenes/default.nim
@@ -1,47 +1,24 @@
 import pixie
-import assets/fonts as fontAssets
-import times
 
 from frameos/config import Config
-import apps/unsplash/frame as unsplashApp
+import apps/unsplash/app as unsplashApp
+import apps/text/app as textApp
 
 type Scene = object
   config: Config
-  image: Image
   app_1: unsplashApp.App
+  app_2: textApp.App
   typeface: Typeface
 
 proc init*(config: Config): Scene =
-  result = Scene()
-  result.config = config
-  result.image = newImage(config.width, config.height)
+  result = Scene(config: config)
   result.app_1 = unsplashApp.init(config, unsplashApp.AppConfig(
       keyword: "random"))
-  let parseTtfTimer = epochTime()
-  result.typeface = parseTtf(fontAssets.getAsset("assets/fonts/Ubuntu-Regular_1.ttf"))
-  echo "Time taken to parse ttf: ", (epochTime() - parseTtfTimer) * 1000, " ms"
+  result.app_2 = textApp.init(config, textApp.AppConfig(
+      text: "Hello"))
 
 proc render*(self: Scene): Image =
-  echo "---- Creating image ----"
-  let image = newImage(self.image.width, self.image.height)
-
+  var image = newImage(self.config.width, self.config.height)
   self.app_1.render(image)
-
-  proc newFont(typeface: Typeface, size: float32, color: Color): Font =
-    result = newFont(typeface)
-    result.size = size
-    result.paint.color = color
-
-  let drawFontTimer = epochTime()
-  let spans = @[
-    newSpan("verb [with object] ",
-      newFont(self.typeface, 12, color(0.78125, 0.78125, 0.78125, 1))),
-    newSpan("unladen strallow\n", newFont(self.typeface, 36, color(0, 0, 0, 1))),
-    newSpan("\nstral·low\n", newFont(self.typeface, 13, color(0, 0.5, 0.953125, 1))),
-    newSpan("\n1. free (something) from restrictive restrictions \"the regulations are intended to strallow changes in public policy\" ",
-        newFont(self.typeface, 14, color(0.3125, 0.3125, 0.3125, 1)))
-  ]
-
-  image.fillText(typeset(spans, vec2(180, 180)), translate(vec2(10, 10)))
-  echo "Time taken to draw text: ", (epochTime() - drawFontTimer) * 1000, " ms"
+  self.app_2.render(image)
   return image
