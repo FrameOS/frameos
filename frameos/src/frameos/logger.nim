bd8b49d07d659a579729de7550ee66f20468a9ab
Marius Andra <marius.andra@gmail.com>
2023-12-30 11:20:09 +0100

    batched logging

--- frameos/src/frameos/logger.nim
+++ frameos/src/frameos/logger.nim
@@ -1,6 +1,17 @@
-import httpclient, zippy, json
+import httpclient, zippy, json, sequtils, os, times
 
 from frameos/types import FrameConfig, Logger
+
+type
+  LoggerThread = ref object
+    frameConfig: FrameConfig
+    client: HttpClient
+    url: string
+    logs: seq[JsonNode]
+    erroredLogs: seq[JsonNode]
+    lastSendAt: float
+
+const LOG_FLUSH_INTERVAL = 1.0
 
 # TODO:
 # - Keep a limited list of logs in memory
@@ -14,18 +25,40 @@ var
 
 channel.open()
 
-proc logInThread*(frameConfig: FrameConfig, payload: JsonNode,
-    client: HttpClient, url: string) =
+proc logInThread*(self: LoggerThread) =
+  var newLogs = self.erroredLogs.concat(self.logs)
+  self.logs = @[]
+  self.erroredLogs = @[]
+  if newLogs.len == 0:
+    return
+  if newLogs.len > 100:
+    newLogs = newLogs[(newLogs.len - 100) .. (newLogs.len - 1)]
   try:
-    let body = %*{"log": payload}
-    let response = client.request(url, httpMethod = HttpPost,
+    let body = %*{"logs": newLogs}
+    let response = self.client.request(self.url, httpMethod = HttpPost,
         body = compress($body))
+    self.lastSendAt = epochTime()
     if response.code != Http200:
       echo "Error sending logs: HTTP " & $response.status
+      self.erroredLogs = self.erroredLogs.concat(newLogs)
   except CatchableError as e:
     echo "Error sending logs: " & $e.msg
+    self.erroredLogs = self.erroredLogs.concat(newLogs)
 
-proc threadRunner(frameConfig: FrameConfig) {.thread.} =
+proc start(self: LoggerThread) =
+  while true:
+    let logCount = (self.logs.len + self.erroredLogs.len)
+    if logCount > 10 or (logCount > 0 and self.lastSendAt + LOG_FLUSH_INTERVAL <
+        epochTime()):
+      self.logInThread()
+
+    let (success, payload) = channel.tryRecv()
+    if success:
+      self.logs.add(payload)
+    else:
+      sleep(100)
+
+proc createThreadRunner(frameConfig: FrameConfig) {.thread.} =
   var client = newHttpClient(timeout = 10000)
   client.headers = newHttpHeaders([
       ("Authorization", "Bearer " & frameConfig.serverApiKey),
@@ -36,15 +69,19 @@ proc threadRunner(frameConfig: FrameConfig) {.thread.} =
       443: "https" else: "http"
   let url = protocol & "://" & frameConfig.serverHost & ":" &
     $frameConfig.serverPort & "/api/log"
-  while true:
-    let payload = channel.recv()
-    logInThread(frameConfig, payload, client, url)
 
-method log*(self: Logger, payload: JsonNode) {.base.} =
-  channel.send(payload)
+  var loggerThread = LoggerThread(
+    frameConfig: frameConfig,
+    client: client,
+    url: url,
+    logs: @[],
+    erroredLogs: @[],
+    lastSendAt: 0.0,
+  )
+  loggerThread.start()
 
 proc newLogger*(frameConfig: FrameConfig): Logger =
-  createThread(thread, threadRunner, frameConfig)
+  createThread(thread, createThreadRunner, frameConfig)
   var logger = Logger(
     frameConfig: frameConfig,
     channel: channel,
