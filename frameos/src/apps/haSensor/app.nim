af522d5203186ee5f5da65203c6cb3cba05c5a88
Marius Andra <marius.andra@gmail.com>
2023-12-31 16:09:29 +0100

    HA nil

--- frameos/src/apps/haSensor/app.nim
+++ frameos/src/apps/haSensor/app.nim
@@ -44,32 +44,35 @@ proc run*(self: App, context: ExecutionContext) =
     return
 
   var client = newHttpClient(timeout = 10000)
-  client.headers = newHttpHeaders([
-      ("Authorization", "Bearer " & accessToken),
-      ("Content-Type", "application/json"),
-      ("Content-Encoding", "gzip")
-  ])
-  let url = &"{haUrl}/api/states/{self.appConfig.entityId}"
+  try:
+    client.headers = newHttpHeaders([
+        ("Authorization", "Bearer " & accessToken),
+        ("Content-Type", "application/json"),
+        ("Content-Encoding", "gzip")
+    ])
+    let url = &"{haUrl}/api/states/{self.appConfig.entityId}"
 
-  if self.json.isNone or self.lastFetchAt == 0 or self.lastFetchAt +
-      self.appConfig.cacheSeconds < epochTime():
-    try:
-      let response = client.request(url)
-      if response.code != Http200:
-        self.error "Error fetching Home Assistant status: HTTP " &
-            $response.status
+    if self.json.isNone or self.lastFetchAt == 0 or self.lastFetchAt +
+        self.appConfig.cacheSeconds < epochTime():
+      try:
+        let response = client.request(url)
+        if response.code != Http200:
+          self.error "Error fetching Home Assistant status: HTTP " &
+              $response.status
+          return
+        self.json = some(parseJson(response.body))
+        self.lastFetchAt = epochTime()
+      except CatchableError as e:
+        self.error "Error fetching Home Assistant status: " & $e.msg
         return
-      self.json = some(parseJson(response.body))
-      self.lastFetchAt = epochTime()
-    except CatchableError as e:
-      self.error "Error fetching Home Assistant status: " & $e.msg
-      return
 
-  if self.json.isSome:
-    let stateKey = if self.appConfig.stateKey ==
-        "": "state" else: self.appConfig.stateKey
-    self.scene.state[stateKey] = self.json.get()
-    if self.appConfig.debug:
-      self.log($self.scene.state[stateKey])
-  else:
-    self.error "No JSON response from Home Assistant"
+    if self.json.isSome:
+      let stateKey = if self.appConfig.stateKey ==
+          "": "state" else: self.appConfig.stateKey
+      self.scene.state[stateKey] = self.json.get()
+      if self.appConfig.debug:
+        self.log($self.scene.state[stateKey])
+    else:
+      self.error "No JSON response from Home Assistant"
+  finally:
+    client.close()
