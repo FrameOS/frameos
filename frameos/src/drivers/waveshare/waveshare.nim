aa5d6a69ef5a30f0966a27ba5dce7fd70ac1ee6e
Marius Andra <marius.andra@gmail.com>
2024-01-01 22:05:31 +0100

    tweaks

--- frameos/src/drivers/waveshare/waveshare.nim
+++ frameos/src/drivers/waveshare/waveshare.nim
@@ -1,32 +1,31 @@
-import pixie, json, linuxfb, posix, strformat, sequtils, osproc, os
+import pixie, json
 
 from frameos/types import FrameOS, Logger, FrameOSDriver
 
 import ePaper/DEV_Config as waveshareConfig
-import ePaper/EPD_7in5_V2 as waveshare
+import ePaper/EPD_7in5_V2 as waveshareDisplay
 
 type Driver* = ref object of FrameOSDriver
   logger: Logger
   width: int
   height: int
 
-
 proc init*(frameOS: FrameOS): Driver =
   let logger = frameOS.logger
-  let width = 800
-  let height = 480
+  let width = waveshareDisplay.EPD_7IN5_V2_WIDTH
+  let height = waveshareDisplay.EPD_7IN5_V2_HEIGHT
 
   let devInitResponse = waveshareConfig.DEV_Module_Init()
   if devInitResponse != 0:
     logger.log(%*{"event": "driver:waveshare", "devInit": "error",
         "responseCode": devInitResponse})
 
-  let initResponse = waveshare.EPD_7IN5_V2_Init()
+  let initResponse = waveshareDisplay.EPD_7IN5_V2_Init()
   if initResponse != 0:
     logger.log(%*{"event": "driver:waveshare", "init": "error",
       "EPD_7IN5_V2_Init": initResponse})
 
-  waveshare.EPD_7IN5_V2_Clear()
+  waveshareDisplay.EPD_7IN5_V2_Clear()
   logger.log(%*{"event": "driver:waveshare", "clear": "done"})
 
   try:
@@ -53,11 +52,8 @@ proc render*(self: Driver, image: Image) =
     for x in 0..<image.width:
       let index = y * image.width + x
       let pixel = image.data[index]
-
       let weightedSum = pixel.r * 299 + pixel.g * 587 + pixel.b * 114
       let bw: uint8 = if weightedSum < 128 * 1000: 0 else: 1
+      packedImage[index div 8] = packedImage[index div 8] or (bw shl (7 - (index mod 8)))
 
-      packedImage[index div 8] = packedImage[index div 8] or (bw shl (index mod 8))
-
-  waveshare.EPD_7IN5_V2_Display(addr packedImage[0])
-  sleep(2000)
+  waveshareDisplay.EPD_7IN5_V2_Display(addr packedImage[0])
