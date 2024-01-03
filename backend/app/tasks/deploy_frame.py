34c27dc9efb86c5a5dd66e2201bc7c6ca7b423b8
Marius Andra <marius.andra@gmail.com>
2024-01-03 02:05:12 +0100

    one more flag

--- backend/app/tasks/deploy_frame.py
+++ backend/app/tasks/deploy_frame.py
@@ -210,7 +210,7 @@ def create_local_build_archive(frame: Frame, build_dir: str, build_id: str, nim_
     # run "nim c --os:linux --cpu:arm64 --compileOnly --genScript --nimcache:tmp/build_1 src/frameos.nim"
     status, out, err = exec_local_command(
         frame,
-        f"cd {source_dir} && nimble install -d && {nim_path} compile --os:linux --cpu:{cpu} --compileOnly --genScript --nimcache:{build_dir} src/frameos.nim 2>&1"
+        f"cd {source_dir} && nimble install -d -y && {nim_path} compile --os:linux --cpu:{cpu} --compileOnly --genScript --nimcache:{build_dir} src/frameos.nim 2>&1"
     )
     if status != 0:
         last_line = [line for line in err.split("\n") if line != ''][-1]
