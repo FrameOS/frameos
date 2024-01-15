15e5ac1668fa806292196432f319617dfc77a2f2
Marius Andra <marius.andra@gmail.com>
2024-01-15 09:54:22 +0100

    plug hole

--- frameos/src/frameos/runner.nim
+++ frameos/src/frameos/runner.nim
@@ -1,8 +1,10 @@
-import json, pixie, times, options, asyncdispatch, locks
+import json, pixie, times, options, asyncdispatch, strformat, std/isolation, locks
+import pixie/fileformats/png
 import scenes/default as defaultScene
 
 import frameos/channels
 import frameos/types
+import frameos/config
 from frameos/utils/image import rotateDegrees, renderError, scaleAndDrawImage
 
 import drivers/drivers as drivers
@@ -15,8 +17,6 @@ type
     frameConfig: FrameConfig
     logger: Logger
     scene: FrameScene
-    lastImage: Option[Image]
-    lastRotatedImage: Option[Image]
     lastRenderAt: float
     sleepFuture: Option[Future[void]]
     isRendering: bool = false
@@ -24,70 +24,43 @@ type
 
 var
   thread: Thread[(FrameConfig, Logger)]
-  globalLastImageLock: Lock
-  globalLastImage {.guard: globalLastImageLock.}: Option[Image]
+  pngLock: Lock
+  pngImage = newImage(1, 1)
 
+proc setLastImage(image: Image) =
+  withLock pngLock:
+    if pngImage.width != image.width or pngImage.height != image.height:
+      pngImage = newImage(image.width, image.height)
+    pngImage.draw(image)
 
-proc renderScene*(self: RunnerThread) =
+proc getLastPng*(): string =
+  var copy: seq[ColorRGBX]
+  withLock pngLock:
+    copy = pngImage.data
+  return encodePng(pngImage.width, pngImage.height, 4, copy[0].addr, copy.len * 4)
+
+proc renderScene*(self: RunnerThread): Image =
   let sceneTimer = epochTime()
-  let requiredWidth = case self.frameConfig.rotate:
-    of 90, 270: self.frameConfig.height
-    else: self.frameConfig.width
-  let requiredHeight = case self.frameConfig.rotate:
-    of 90, 270: self.frameConfig.width
-    else: self.frameConfig.height
-  self.logger.log(%*{"event": "render", "width": requiredWidth,
-      "height": requiredHeight})
+  let requiredWidth = self.frameConfig.renderWidth()
+  let requiredHeight = self.frameConfig.renderHeight()
+  self.logger.log(%*{"event": "render", "width": requiredWidth, "height": requiredHeight})
 
   try:
-    # render the scene
-    type DefaultScene = defaultScene.Scene
-    let image = defaultScene.render(self.scene.DefaultScene)
-
-    # do we have to resize the result?
+    let image = defaultScene.render(defaultScene.Scene(self.scene))
     if image.width != requiredWidth or image.height != requiredHeight:
       let resizedImage = newImage(requiredWidth, requiredHeight)
-      resizedImage.scaleAndDrawImage(image, self.frameConfig.scalingMode)
-      withLock globalLastImageLock:
-        globalLastImage = some(resizedImage)
-      self.lastImage = some(resizedImage)
-      self.lastRotatedImage = some(resizedImage.rotateDegrees(
-          self.frameConfig.rotate))
+      resizedImage.fill(self.frameConfig.backgroundColor)
+      scaleAndDrawImage(resizedImage, image, self.frameConfig.scalingMode)
+      setLastImage(resizedImage)
+      result = resizedImage.rotateDegrees(self.frameConfig.rotate)
     else:
-      withLock globalLastImageLock:
-        globalLastImage = some(image)
-      self.lastImage = some(image)
-      self.lastRotatedImage = some(image.rotateDegrees(
-          self.frameConfig.rotate))
+      setLastImage(image)
+      result = image.rotateDegrees(self.frameConfig.rotate)
   except Exception as e:
-    self.logger.log(%*{"event": "render:error", "error": $e.msg,
-        "stacktrace": e.getStackTrace()})
+    result = renderError(requiredWidth, requiredHeight, &"Error: {$e.msg}\n{$e.getStackTrace()}")
+    self.logger.log(%*{"event": "render:error", "error": $e.msg, "stacktrace": e.getStackTrace()})
   self.lastRenderAt = epochTime()
-  self.logger.log(%*{"event": "render:done", "ms": round((epochTime() -
-      sceneTimer) * 1000, 3)})
-
-proc lastRender*(self: RunnerThread): Image =
-  if self.lastImage.isSome:
-    result = self.lastImage.get()
-  else:
-    case self.frameConfig.rotate:
-      of 90, 270:
-        result = renderError(self.frameConfig.height, self.frameConfig.width, "Error: No image rendered yet")
-      else:
-        result = renderError(self.frameConfig.width, self.frameConfig.height, "Error: No image rendered yet")
-    self.lastImage = some(result)
-
-proc lastRotatedRender*(self: RunnerThread): Image =
-  if self.lastRotatedImage.isSome:
-    result = self.lastRotatedImage.get()
-  else:
-    case self.frameConfig.rotate:
-      of 90, 270:
-        result = renderError(self.frameConfig.height, self.frameConfig.width, "Error: No image rendered yet")
-      else:
-        result = renderError(self.frameConfig.width, self.frameConfig.height, "Error: No image rendered yet")
-    result = result.rotateDegrees(self.frameConfig.rotate)
-    self.lastRotatedImage = some(result)
+  self.logger.log(%*{"event": "render:done", "ms": round((epochTime() - sceneTimer) * 1000, 3)})
 
 proc startRenderLoop*(self: RunnerThread): Future[void] {.async.} =
   self.logger.log(%*{"event": "startRenderLoop"})
@@ -102,9 +75,8 @@ proc startRenderLoop*(self: RunnerThread): Future[void] {.async.} =
     timer = epochTime()
     self.isRendering = true
     self.scene.isRendering = true
-    self.triggerRenderNext = false
-    self.renderScene()
-
+    self.triggerRenderNext = false # used to debounce render events received while rendering
+    let lastRotatedImage = self.renderScene()
     if self.frameConfig.interval < 1:
       let now = epochTime()
       if now >= nextServerRenderAt:
@@ -116,12 +88,11 @@ proc startRenderLoop*(self: RunnerThread): Future[void] {.async.} =
       triggerServerRender()
 
     driverTimer = epochTime()
-    # TODO: render the part in another thread if fast rendering is enabled
-    drivers.render(self.lastRotatedRender())
+    # TODO: render the driver part in another thread if fast rendering is enabled
+    drivers.render(lastRotatedImage)
 
     self.logger.log(%*{"event": "render:driver",
-        "device": self.frameConfig.device, "ms": round((epochTime() -
-            driverTimer) * 1000, 3)})
+        "device": self.frameConfig.device, "ms": round((epochTime() - driverTimer) * 1000, 3)})
 
     if self.frameConfig.interval < 1:
       let now = epochTime()
@@ -129,9 +100,8 @@ proc startRenderLoop*(self: RunnerThread): Future[void] {.async.} =
         fastSceneCount += 1
         # Two fast scenes in a row
         if fastSceneCount == 2:
-          # TODO: still log if there was a slow scene while logging was stopped
-          self.logger.log(%*{"event": "pause",
-              "message": "Rendering fast. Pausing logging for 10s"})
+          # TODO: capture logs per scene and log if slow
+          self.logger.log(%*{"event": "pause", "message": "Rendering fast. Pausing logging for 10s"})
           self.logger.disable()
           fastSceneResumeAt = now + 10
         elif fastSceneResumeAt != 0.0 and now > fastSceneResumeAt:
@@ -142,7 +112,6 @@ proc startRenderLoop*(self: RunnerThread): Future[void] {.async.} =
         fastSceneCount = 0
         fastSceneResumeAt = 0.0
         self.logger.enable()
-
 
     # Gives a chance for the gathered events to be collected
     await sleepAsync(0.001)
@@ -155,12 +124,10 @@ proc startRenderLoop*(self: RunnerThread): Future[void] {.async.} =
       continue
 
     # Sleep until the next frame
-    sleepDuration = max((self.frameConfig.interval - (epochTime() - timer)) *
-        1000, 0.1)
+    sleepDuration = max((self.frameConfig.interval - (epochTime() - timer)) * 1000, 0.1)
     self.logger.log(%*{"event": "sleep", "ms": round(sleepDuration, 3)})
     # Calculate once more to subtract the time it took to log the message
-    sleepDuration = max((self.frameConfig.interval - (epochTime() - timer)) *
-        1000, 0.1)
+    sleepDuration = max((self.frameConfig.interval - (epochTime() - timer)) * 1000, 0.1)
     let future = sleepAsync(sleepDuration)
     self.sleepFuture = some(future)
     await future
@@ -170,8 +137,7 @@ proc triggerRender*(self: RunnerThread): void =
   if self.sleepFuture.isSome:
     self.sleepFuture.get().complete()
   else:
-    self.logger.log(%*{"event": "render",
-        "error": "Render already in progress, ignoring."})
+    self.logger.log(%*{"event": "render", "error": "Render already in progress, ignoring."})
 
 proc dispatchSceneEvent*(self: RunnerThread, event: string, payload: JsonNode) =
   var context = ExecutionContext(
@@ -205,10 +171,8 @@ proc startMessageLoop*(self: RunnerThread): Future[void] {.async.} =
             drivers.turnOff()
           of "mouseMove":
             if self.frameConfig.width > 0 and self.frameConfig.height > 0:
-              payload["x"] = %*((self.frameConfig.width.float * payload[
-                  "x"].getInt().float / 32767.0).int)
-              payload["y"] = %*((self.frameConfig.height.float * payload[
-                  "y"].getInt().float / 32767.0).int)
+              payload["x"] = %*((self.frameConfig.width.float * payload["x"].getInt().float / 32767.0).int)
+              payload["y"] = %*((self.frameConfig.height.float * payload["y"].getInt().float / 32767.0).int)
             self.dispatchSceneEvent(event, payload)
           of "mouseUp", "mouseDown", "keyUp", "keyDown":
             self.dispatchSceneEvent(event, payload)
@@ -239,35 +203,19 @@ proc createThreadRunner*(args: (FrameConfig, Logger)) =
       frameConfig: args[0],
       logger: args[1],
       scene: scene,
-      lastImage: none(Image),
-      lastRotatedImage: none(Image),
       lastRenderAt: 0,
       sleepFuture: none(Future[void]),
     )
     waitFor runnerThread.startRenderLoop() and runnerThread.startMessageLoop()
 
-proc getLastImage*(self: RunnerControl): Image =
-  withLock(globalLastImageLock):
-    if globalLastImage.isNone:
-      case self.frameConfig.rotate:
-        of 90, 270:
-          result = renderError(self.frameConfig.height, self.frameConfig.width,
-            "Error: No image rendered yet")
-        else:
-          result = renderError(self.frameConfig.width, self.frameConfig.height,
-            "Error: No image rendered yet")
-    else:
-      result = globalLastImage.get()
-
 proc newRunner*(frameConfig: FrameConfig, logger: Logger): RunnerControl =
   var runner = RunnerControl(
     logger: logger,
     frameConfig: frameConfig,
-    start: proc () = createThread(thread, createThreadRunner, (
-      frameConfig, logger)),
-    sendEvent: proc (event: string, payload: JsonNode) = eventChannel.send((
-        event, payload)),
+    start: proc () = createThread(thread, createThreadRunner, (frameConfig, logger)),
+    sendEvent: proc (event: string, payload: JsonNode) = eventChannel.send((event, payload)),
   )
+  setLastImage(renderError(frameConfig.renderWidth(), frameConfig.renderHeight(), "FrameOS booting..."))
   return runner
 
 proc triggerRender*(self: RunnerControl): void =
