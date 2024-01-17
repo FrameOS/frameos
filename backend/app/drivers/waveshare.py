80581989c1c49f5ee472ad8ea1019f44e28d9212
Marius Andra <marius.andra@gmail.com>
2024-01-17 16:22:52 +0100

    Update waveshare.py

--- backend/app/drivers/waveshare.py
+++ backend/app/drivers/waveshare.py
@@ -144,7 +144,7 @@ def convert_waveshare_source(variant_key: str) -> WaveshareVariant:
                     variant.init_returns_zero = "(): UBYTE" in line
                 if proc_name.lower() == f"{variant.prefix}_Clear".lower() and variant.clear_function is None:
                     variant.clear_function = proc_name
-                    if "UBYTE color" in line:
+                    if "color: UBYTE" in line:
                         variant.clear_args = "1"
                 if proc_name.lower() == f"{variant.prefix}_4Gray_Clear".lower():
                     variant.clear_function = proc_name
