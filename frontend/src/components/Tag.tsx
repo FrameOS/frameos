71091b97cc5690634f7ffa48a2a4576f957bf349
Marius Andra <marius.andra@gmail.com>
2024-02-25 01:19:37 +0100

    components

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,17 +1,18 @@
 import clsx from 'clsx'
 
-interface TagProps {
+interface TagProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
   className?: string
 }
 
-export function Tag({ children, className }: TagProps) {
+export function Tag({ children, className, ...props }: TagProps) {
   return (
     <div
       className={clsx(
         'inline-block px-1 py-0.5 text-xs font-normal text-gray-400 bg-gray-800 rounded-md border border-gray-700 uppercase align-middle',
         className
       )}
+      {...props}
     >
       {children}
     </div>
