681945934c8f95326ab64a740ecd4233a4ddfa65
Marius Andra <marius.andra@gmail.com>
2024-02-29 00:11:22 +0100

    more hints

--- backend/app/models/frame.py
+++ backend/app/models/frame.py
@@ -131,6 +131,7 @@ def new_frame(name: str, frame_host: str, server_host: str, device: Optional[str
     new_log(frame.id, "welcome", "-> Press 'Save & Deploy' to deploy the default scene")
     new_log(frame.id, "welcome", "-> Install additional scenes in the 'Scenes' panel")
     new_log(frame.id, "welcome", "-> Drag 'Apps' and 'Events' onto the active scene to customize")
+    new_log(frame.id, "welcome", "-> Click 'Edit App' to clone an app and edit its source")
 
     return frame
 
