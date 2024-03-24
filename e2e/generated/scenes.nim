bcc7271c1bc02504a3996a3cbb0fa9bba59906a3
Marius Andra <marius.andra@gmail.com>
2024-03-24 01:39:12 +0100

    color

--- e2e/generated/scenes.nim
+++ e2e/generated/scenes.nim
@@ -2,15 +2,19 @@
 
 import frameos/types
 import tables
+import scenes/scene_color as scene_color
+import scenes/scene_gradient as scene_gradient
 import scenes/scene_image as scene_image
 import scenes/scene_imageError as scene_imageError
 import scenes/scene_qr as scene_qr
 import scenes/scene_qrboard as scene_qrboard
 import scenes/scene_text as scene_text
 
-let defaultSceneId* = "image".SceneId
+let defaultSceneId* = "color".SceneId
 
 const sceneOptions* = [
+  ("color".SceneId, "Test Color"),
+  ("gradient".SceneId, "Test Gradient"),
   ("image".SceneId, "TEST"),
   ("imageError".SceneId, "TEST"),
   ("qr".SceneId, "Test QR"),
@@ -20,6 +24,8 @@ const sceneOptions* = [
 
 proc getExportedScenes*(): Table[SceneId, ExportedScene] =
   result = initTable[SceneId, ExportedScene]()
+  result["color".SceneId] = scene_color.exportedScene
+  result["gradient".SceneId] = scene_gradient.exportedScene
   result["image".SceneId] = scene_image.exportedScene
   result["imageError".SceneId] = scene_imageError.exportedScene
   result["qr".SceneId] = scene_qr.exportedScene
