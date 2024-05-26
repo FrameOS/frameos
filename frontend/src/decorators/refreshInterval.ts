33bb988a9c6b6aa89f531df720a6bef95163e913
Marius Andra <marius.andra@gmail.com>
2024-05-26 23:50:23 +0200

    tweaks

--- frontend/src/decorators/refreshInterval.ts
+++ frontend/src/decorators/refreshInterval.ts
@@ -1,3 +1,9 @@
 export function showAsFps(refreshInterval: number): string {
-  return refreshInterval > 1 ? `${refreshInterval} sec` : `${Math.round((1 / refreshInterval) * 10) / 10} fps`
+  return refreshInterval > 3599
+    ? `${Math.round((refreshInterval / 3600) * 10) / 10} hour${refreshInterval > 3779 ? 's' : ''}`
+    : refreshInterval > 59
+    ? `${Math.round((refreshInterval / 60) * 10) / 10} min`
+    : refreshInterval >= 1
+    ? `${refreshInterval} sec`
+    : `${Math.round((1 / refreshInterval) * 10) / 10} fps`
 }
--- frontend/src/scenes/frame/panels/Diagram/EventNode.tsx
+++ frontend/src/scenes/frame/panels/Diagram/EventNode.tsx
@@ -11,7 +11,7 @@ import { ClipboardIcon } from '@heroicons/react/24/solid'
 import { frameLogic } from '../../frameLogic'
 import { Tooltip } from '../../../../components/Tooltip'
 import { SceneSettings } from '../Scenes/SceneSettings'
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline'
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline'
 import { buttonColor, buttonSize } from '../../../../components/Button'
 import { showAsFps } from '../../../../decorators/refreshInterval'
 
@@ -52,28 +52,39 @@ export function EventNode(props: NodeProps): JSX.Element {
         )}
       >
         <div>{keyword}</div>
-        <Handle
-          type="source"
-          position={Position.Right}
-          id="next"
-          style={{
-            position: 'relative',
-            transform: 'none',
-            width: 20,
-            height: 20,
-            right: 0,
-            top: 0,
-            background: 'rgba(200, 200, 200, 0.8)',
-            borderBottomLeftRadius: 0,
-            borderTopLeftRadius: 0,
-          }}
-        />
+        <div className="flex items-center justify-center gap-2">
+          {scene?.id ? (
+            <Tooltip
+              tooltipColor="gray"
+              className={clsx(buttonSize('tiny'), buttonColor('none'))}
+              title={<SceneSettings sceneId={scene?.id} />}
+            >
+              <EllipsisHorizontalIcon className="w-5 h-5" aria-label="Menu" />
+            </Tooltip>
+          ) : null}
+          <Handle
+            type="source"
+            position={Position.Right}
+            id="next"
+            style={{
+              position: 'relative',
+              transform: 'none',
+              width: 20,
+              height: 20,
+              right: 0,
+              top: 0,
+              background: 'rgba(200, 200, 200, 0.8)',
+              borderBottomLeftRadius: 0,
+              borderTopLeftRadius: 0,
+            }}
+          />
+        </div>
       </div>
       {keyword === 'render' ? (
         // show a blank box with the dimensions of the scene
         <div className="p-1">
           <div
-            className="relative flex items-center justify-center text-center bg-gray-800 text-white text-2xl border border-gray-700 rounded-md"
+            className="relative flex flex-col items-center justify-center text-center bg-gray-800 text-white border border-gray-700 rounded-md"
             style={{
               aspectRatio: `${width} / ${height}`,
               minWidth: 200,
@@ -83,21 +94,12 @@ export function EventNode(props: NodeProps): JSX.Element {
               textShadow: `-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black, 0 0 5px black`,
             }}
           >
-            {scene?.id ? (
-              <div className="absolute top-0 right-0">
-                <Tooltip
-                  tooltipColor="gray"
-                  className={clsx(buttonSize('tiny'), buttonColor('secondary'))}
-                  title={<SceneSettings sceneId={scene?.id} />}
-                >
-                  <AdjustmentsHorizontalIcon className="w-5 h-5" />
-                </Tooltip>
+            <div className="text-2xl mb-1">{`${width}x${height}`}</div>
+            {scene?.settings?.refreshInterval ? (
+              <div className="text-xl">
+                {(scene.settings.refreshInterval >= 1 ? 'refresh: ' : '') + showAsFps(scene.settings.refreshInterval)}
               </div>
             ) : null}
-
-            {`${width}x${height}`}
-            <br />
-            {scene?.settings?.refreshInterval ? showAsFps(scene.settings.refreshInterval) : null}
           </div>
         </div>
       ) : null}
