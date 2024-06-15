331cd2eee848f0c5ccbaf5dcb19c3af59d9b2e6c
Marius Andra <marius.andra@gmail.com>
2024-06-16 01:22:33 +0200

    add type

--- frontend/src/components/FieldTypeTag.tsx
+++ frontend/src/components/FieldTypeTag.tsx
@@ -3,21 +3,26 @@ import { Tag, TagProps } from './Tag'
 
 export const typeColors: Record<FieldType, TagProps['color']> = {
   string: 'blue',
-  float: 'red',
-  integer: 'orange',
+  float: 'teal',
+  integer: 'teal',
   boolean: 'gray',
-  color: 'secondary',
+  color: 'orange',
   json: 'secondary',
   node: 'secondary',
   scene: 'secondary',
-  image: 'primary',
+  image: 'red',
 }
 
 export interface FieldTypeTagProps {
   type: FieldType | AppConfigFieldType
+  className?: string
 }
 
-export function FieldTypeTag({ type }: FieldTypeTagProps): JSX.Element {
+export function FieldTypeTag({ type, className }: FieldTypeTagProps): JSX.Element {
   const _type = toFieldType(type)
-  return <Tag color={typeColors[_type]}>{_type}</Tag>
+  return (
+    <Tag color={typeColors[_type]} className={className}>
+      {_type}
+    </Tag>
+  )
 }
--- frontend/src/scenes/frame/panels/Apps/Apps.tsx
+++ frontend/src/scenes/frame/panels/Apps/Apps.tsx
@@ -5,6 +5,7 @@ import { H6 } from '../../../../components/H6'
 import { appsLogic } from './appsLogic'
 import { TextInput } from '../../../../components/TextInput'
 import React from 'react'
+import { FieldTypeTag } from '../../../../components/FieldTypeTag'
 
 export function Apps() {
   const { appsByCategory, search } = useValues(appsLogic)
@@ -19,16 +20,21 @@ export function Apps() {
       {Object.entries(appsByCategory).map(([category, apps]) => (
         <div className="space-y-2" key={category}>
           <H6 className="capitalize">{categoryLabels[category] ?? category}</H6>
-          {Object.entries(apps).map(([keyword, { name, description }]) => (
+          {Object.entries(apps).map(([keyword, app]) => (
             <Box
               key={keyword}
-              className="bg-gray-900 px-3 py-2 dndnode flex items-center justify-between space-x-2 cursor-move"
+              className="bg-gray-900 px-3 py-2 dndnode flex items-center justify-between space-x-2 cursor-move w-full"
               draggable
               onDragStart={(event) => onDragStart(event, keyword)}
             >
-              <div>
-                <H6>{name}</H6>
-                <div className="text-sm">{description}</div>
+              <div className="w-full">
+                <div className="flex items-start justify-between">
+                  <H6>{app.name}</H6>
+                  {app.output?.map((output) => (
+                    <FieldTypeTag className="mt-1" type={output.type} />
+                  ))}
+                </div>
+                <div className="text-sm">{app.description}</div>
               </div>
             </Box>
           ))}
