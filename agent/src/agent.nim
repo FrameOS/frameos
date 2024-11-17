cefccbef2b7faf61e5df8e9702ab373d594219ce
Marius Andra <marius.andra@gmail.com>
2024-11-17 23:03:51 +0100

    send frame data

--- agent/src/agent.nim
+++ agent/src/agent.nim
@@ -1,22 +1,25 @@
 import asyncdispatch, ws, json, strformat
 
 proc main() {.async.} =
-  var attmepts = 0
+  var attempts = 0
   while true:
     try:
       let data = parseFile("./frame.json")
       let serverHost = data{"serverHost"}.getStr()
       let serverPort = data{"serverPort"}.getInt()
       let url = &"ws://{serverHost}:{serverPort}/ws/agent"
-
+      let welcomePayload = %*{
+        "apiKey": data{"serverApiKey"}.getStr(),
+        "frameHost": data{"frameHost"}.getStr(),
+        "framePort": data{"framePort"}.getInt(),
+      }
       echo &"Connecting to {url}"
       var ws = await newWebSocket(url)
       echo "Connected"
-
+      await ws.send($welcomePayload)
+      echo "Sent welcome message"
       while true:
-        echo "Sending message"
-        await ws.send("Welcome to simple echo server")
-        echo "Receiving message"
+        echo "Waiting for message..."
         echo await ws.receiveStrPacket()
       ws.close()
     except IOError as e:
@@ -25,8 +28,10 @@ proc main() {.async.} =
     except Exception as e:
       echo "Error"
       echo e.msg
-    attmepts += 1
-    echo &"Retrying in {attmepts} seconds"
-    await sleepAsync(attmepts * 1000)
+
+    if attempts < 60:
+      attempts += 1
+    echo &"Retrying in {attempts} seconds"
+    await sleepAsync(attempts * 1000)
 
 waitFor main()
