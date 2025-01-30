b75e941ddb7196b442b78d2e58aa559bd43fd462
Marius Andra <marius.andra@gmail.com>
2025-01-30 01:01:37 +0100

    flip the options

--- frontend/src/scenes/frame/panels/SceneState/sceneStateLogic.tsx
+++ frontend/src/scenes/frame/panels/SceneState/sceneStateLogic.tsx
@@ -80,7 +80,7 @@ export const sceneStateLogic = kea<sceneStateLogicType>([
     },
     addField: () => {
       const oldFields = values.scene?.fields ?? []
-      actions.setFields([...oldFields, { name: '', label: '', type: 'string', persist: 'memory', access: 'private' }])
+      actions.setFields([...oldFields, { name: '', label: '', type: 'string', persist: 'disk', access: 'public' }])
       actions.editField(oldFields.length)
     },
   })),
