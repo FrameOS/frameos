3495be50990f6f79654220d95dfc747b42fa23a6
Marius Andra <marius.andra@gmail.com>
2025-11-21 00:48:09 +0100

    oops

--- backend/app/utils/cross_compile.py
+++ backend/app/utils/cross_compile.py
@@ -223,7 +223,7 @@ class CrossCompiler:
                 log_debug "Container uname: $(uname -a)"
                 log_debug "Working directory before build: $(pwd)"
 
-                log_debug "ls -l /sysroot/usr/local/lib
+                log_debug "ls -l /sysroot/usr/local/lib"
                 ls -l /sysroot/usr/local/lib
 
                 extra_cflags={extra_cflags}
