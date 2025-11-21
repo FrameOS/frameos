61475e3a6302de6608e0ac76c6cc7fbc29a15208
Marius Andra <marius.andra@gmail.com>
2025-11-21 02:00:54 +0100

    remove smoke test for now

--- frameos/src/frameos.nim
+++ frameos/src/frameos.nim
@@ -6,6 +6,6 @@ from ./frameos/frameos import startFrameOS
 when isMainModule:
   let args = commandLineParams()
   if args.len > 0 and args[0] == "check":
-    echo "ok"
+    echo "FrameOS check: passed 🎉"
   else:
     waitFor startFrameOS() # blocks forever
