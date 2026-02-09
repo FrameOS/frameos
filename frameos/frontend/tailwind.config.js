9d9340c8e694bde04480df391b522ea325d54e37
Marius Andra <marius.andra@gmail.com>
2026-02-09 01:24:05 +0100

    tailwind

--- frameos/frontend/tailwind.config.js
+++ frameos/frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './src/index.html'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/index.html',
+    '../../frontend/src/**/*.{js,ts,jsx,tsx}',
+    '../../frontend/src/index.html',
+  ],
   theme: {
     extend: {},
   },
