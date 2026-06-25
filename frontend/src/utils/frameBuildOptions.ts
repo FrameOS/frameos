8c60aeb15e4ee21910a847877a3d0ec4794baad1
Marius Andra <marius.andra@gmail.com>
2026-06-25 08:56:42 +0200

    text

--- frontend/src/utils/frameBuildOptions.ts
+++ frontend/src/utils/frameBuildOptions.ts
@@ -10,7 +10,7 @@ export interface FrameBuildOption<T extends string = string> {
 
 export const frameCompilationModeOptions: FrameBuildOption<FrameCompilationModeOptionValue>[] = [
   { value: '', label: 'Prefer binaries, build from source if needed' },
-  { value: 'precompiled', label: `Install binaries (version ${versions.frameos.split('+')[0]})` },
+  { value: 'precompiled', label: `Install precompiled binaries (version ${versions.frameos.split('+')[0]})` },
   { value: 'static', label: 'Build from source - single binary' },
   { value: 'shared', label: 'Build from source - drivers and scenes separately' },
   { value: 'shared-scenes', label: 'Build from source - drivers separately, scenes combined' },
